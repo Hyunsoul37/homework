b4ce9b42d9206869e52bd3276a9183c08584b3cf
Hyunsoul37 <49974797+Hyunsoul37@users.noreply.github.com>
2019-04-25 20:31:51 +0900

    주석 달은 CPP
    주석을 생활화 하자

--- hw_ClassSample/Aircondition/Aircondition.cpp
+++ hw_ClassSample/Aircondition/Aircondition.cpp
@@ -8,24 +8,24 @@ class Airconditional
 public:
 	Airconditional() { }
 
-	bool PowerCheck() const { return power; }
-	void SetPower() { power = !power; }
+	bool PowerCheck() const { return power; }		// 전원 ON / OFF 확인합니다.
+	void SetPower() { power = !power; }				// 전원을 반대로 만듭니다.
 
 	void SetDegree(int C) { degree = C; }
-	int GetDegree() const { return degree; }
+	int GetDegree() const { return degree; }		//출력만 하기에 const를 붙혔습니다.
 
-	void DegreeControl()
+	void DegreeControl()							//에어컨이 켜지면 일정 시간 뒤 온도가 내려가도록 했습니다.
 	{
 		bool power = PowerCheck();
 
-		if (power == false)
+		if (power == false)							//전원이 OFF면 작동하지 않습니다.
 		{
 			return;
 		}
 
 		while (1)
 		{
-			if (GetTickCount() % 10000 == 0)
+			if (GetTickCount() % 10000 == 0)		//쓰는법은 잘모르나 검색해서 썼습니다.
 			{
 				system("cls");
 				cout << "Now Degree : " << degree << endl;
@@ -37,20 +37,20 @@ public:
 
 
 private:
-	bool power = false;
+	bool power = false;								//false면 OFF, true면 ON으로 설정 했습니다.
 	int degree = 0;
-	size_t Time = 0;
+	size_t Time = 0;								//시간 값인데 마이너스는 안나올꺼같아 size_t로 선언 했습니다.
 };
 
 int main()
 {
 	Airconditional air;
 
-	air.SetDegree(20);
+	air.SetDegree(20);								
 	air.SetPower();
 	air.DegreeControl();
 
 
-	getchar();
+	getchar();										//화이팅!
 	return 0;
 }
--- hw_ClassSample/Aircondition/Phone.cpp
+++ hw_ClassSample/Aircondition/Phone.cpp
@@ -4,9 +4,9 @@ class Phone
 {
 public:
 	Phone(const char * Maker, const char * Moduler, const char * Color)
-		: m_pMaker(Maker), m_pModuler(Moduler), m_nColor(Color)	{ }
+		: m_pMaker(Maker), m_pModuler(Moduler), m_nColor(Color)	{ }			//생성할때 모든정보를 저장합니다.
 
-	~Phone()
+	~Phone()												//포인터 값 초기화 및 할당시 free시켜줍니다.
 	{
 		m_nColor   = nullptr;
 		m_pMaker   = nullptr;
@@ -17,8 +17,8 @@ public:
 		delete m_pModuler;
 	}
 
-	void Info() const
-	{
+	void Info() const										//출력만 하는 함수라 const 붙혔습니다.
+	{														//정보를 출력해 줍니다.
 		cout << "Maker : " << m_pMaker << endl;
 		cout << "Moduler : " << m_pModuler << endl;
 		cout << "Color : " << m_nColor << endl;
@@ -27,11 +27,11 @@ public:
 		cout << endl;
 	}
 	
-	void SetPower()	{ m_nPower = !m_nPower;	}
+	void SetPower()	{ m_nPower = !m_nPower;	}				//power를 반대로 만들어 주는 함수입니다.
 
 	
 private:
-	bool m_nPower = false;
+	bool m_nPower = false;									//기본으로 OFF되어있고 true는 ON입니다.
 	const char * m_nColor = nullptr;
 	const char * m_pMaker = nullptr;
 	const char * m_pModuler = nullptr;
@@ -40,7 +40,7 @@ private:
 
 int main()
 {
-	Phone phone ("Apple", "iPhone X", "Black");
+	Phone phone ("Apple", "iPhone X", "Black");				//아이폰 짱짱 아이폰 쓰세여
 	Phone phone1 ("Samsung", "galaxy S10", "White");
 
 	phone.Info();
@@ -51,6 +51,6 @@ int main()
 	phone.Info();
 	phone1.Info();
 
-	getchar();
+	getchar();												//화이팅
 	return 0;
 }
