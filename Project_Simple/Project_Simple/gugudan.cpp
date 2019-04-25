4a40de250b000def168cde71df8c7c70037ec16e
Hyunsoul37 <49974797+Hyunsoul37@users.noreply.github.com>
2019-04-25 17:05:24 +0900

    구구단X구구단
    구구단구구구구구구구구구구구구구

---
+++ Project_Simple/Project_Simple/gugudan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int i, j;
+
+	for (i = 2; i <= 9;i++)
+	{
+		for (j = 1; j <= 9;j++)
+		{
+			cout << i << " * " << j << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
+
+	getchar();
+}
